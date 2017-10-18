1ffe87eb342180aa71eda3b15f085a621c517161
Junha Kim <kimjh4930@gmail.com>
2017-10-18 10:07:08 +0000

    fix errors.

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ function connectDB(){
 var authUser = function(database, id, password, callback){
 	console.log('authUser 호출됨.');
 	
-	var users = database.collection('uses');
+	var users = database.collection('users');
 	
 	//아이디와 비밀번호를 사용해 검색.
 	users.find({"id" : id, "password" : password}).toArray(function(err, docs){
@@ -83,21 +83,22 @@ app.post('/process/login', function(req,res){
 				res.write('<h1>로그인 성공.</h1>');
 				res.write('<div><p>사용자 아이디 : ' + paramId + '</p></div>');
 				res.write('<div><p>사용자 이름 : ' + docs[0].name + '</p></div>');
-				res.write("<br><br><a href='/public/login.html'>다시 로그인하기</a>");
+				res.write("<br><br><a href='/login.html'>다시 로그인하기</a>");
 				res.end();
 			}else{
 				res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
 				res.write('<h1>로그인 실패</h1>');
 				res.write('<div><p>아이디와 비밀번호를 다시 확인하십시오.</p></div>');
-				res.write("<br><br><a href='/public/login.html'>다시 로그인하기.</a>");
+				res.write("<br><br><a href='/login.html'>다시 로그인하기.</a>");
 				res.end();
 			}
 			
-			res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-			res.write('<h2>데이터베이스 연결 실패</h2>');
-			res.write('<div><p>데이터베이스에 연결하지 못했습니다.</p></div>');
-			res.end();
 		});
+	}else{
+		res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+		res.write('<h2>데이터베이스 연결 실패.</h2>');
+		res.write('<div><p>데이터베이스에 연결하지 못했습니다.</p></div>');
+		res.end();
 	}
 });
 

c715a3dfcef3981568ab8f01723a61ed3cf81340
Kim, Junha <kimjh4930@gmail.com>
2017-10-20 21:09:05 +0900

    test

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ module.exports = {
 			{file:'./database/user_schema', collection:'user3', schemaName:'UserSchema', modelName:'UserModel'}
 		],
 		route_info : [
-			{file : './user', path : '/process/login', method : 'login', type : 'post'},
-			{file : './user', path : '/process/adduser', method : 'adduser', type : 'post'},
-			{file : './user', path : '/process/listuser', method : 'listuser', type : 'post'}
+			{file : './routes/user', path : '/process/login', method : 'login', type : 'post'},
+			{file : './routes/user', path : '/process/adduser', method : 'adduser', type : 'post'},
+			{file : './routes/user', path : '/process/listuser', method : 'listuser', type : 'post'}
 		]
 		
 	
